8f977f3fae281400b663799f9cad46cbe76d700b
Omar Rosas <jorosas@gmail.com>
2020-07-28 09:33:27 -0500

    Código al 100% con fetch API

--- main.js
+++ main.js
@@ -3,23 +3,44 @@ let factText = document.getElementById('factText');
 
 let numberInput = document.getElementById('numerInput');
 
-numberInput.addEventListener('input', getFactAjax);
+// Nota: Se llama a función AJAX con XMLHttpRequest
+// numberInput.addEventListener('input', getFactAjax);
+numberInput.addEventListener('input', getFactFetch);
 
-function getFactAjax() {
+// Nota: Función AJAX, utilizando XMLHttpRequest
+// function getFactAjax() {
+//     let number = numberInput.value;
+
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', `http://numbersapi.com/${number}`);
+
+//     xhr.onload = function () {
+//         if(this.status == 200 && number!= ''){
+//             console.log(this.responseText);
+//             factDiv.style.display = 'block';
+
+//             factText.innerHTML = this.responseText;
+//         }
+//     }
+//     xhr.send();
+// }
+
+function getFactFetch() {
     let number = numberInput.value;
+    console.log(number);
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://numbersapi.com/${number}`);
+    fetch(`http://numbersapi.com/${number}`)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            if (number != '') {
+                factDiv.style.display = 'block';
+                // Nota: No funciona con innerHTML
+                factText.innerText = data;
+            }
+        })
+        .catch(err => console.log(err));
 
-    xhr.onload = function () {
-        if(this.status == 200 && number!= ''){
-            console.log(this.responseText);
-            factDiv.style.display = 'block';
-
-            factText.innerHTML = this.responseText;
-        }
-    }
-    xhr.send();
 }
 
 console.log(factDiv);
